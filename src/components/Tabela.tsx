f8ab075e16fea65d17157551aa3dd1183e93a9b6
Andre Krug <baurudoporto@gmail.com>
2022-10-10 21:08:46 -0300

    aualizacao-recados

--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -7,10 +7,11 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Button, Container, Dialog, DialogTitle, Grid, IconButton, TextField, Typography } from '@mui/material';
+import { Button, Container, Dialog, DialogTitle, Grid, IconButton, TextField, ToggleButton, Typography } from '@mui/material';
 import { Stack } from '@mui/system';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from '@mui/icons-material/Edit';
+import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
 import { api, get } from '../service/api/jasonplaceholder/toDos';
 import axios from 'axios';
 
@@ -52,7 +53,8 @@ export default function Tabela() {
   const [descricao, setDescricao] = useState('');
 
 
-
+ const[listArq, setlistArq] = useState([]);
+  const[btnArq, setBtnArq] = useState(false);
 
 
 
@@ -120,6 +122,27 @@ export default function Tabela() {
 
   }
 
+  function arquivado(id:number, titulo:string, descricao:string){
+
+ let x = JSON.parse(localStorage.getItem('arquivado') || '[]');
+
+  const novoArquivo = {
+                      id: `ARQ-${id}`,
+                      titulo:titulo,
+                      descricao:descricao
+                    }
+
+                    x.push(novoArquivo);
+
+    localStorage.setItem('arquivado', JSON.stringify(x))
+    deletarRecado(id);
+    alert("Recado arquivado com sucesso!");
+
+
+
+
+  }
+
 
   function deletarRecado(id: number) {
     axios
@@ -136,6 +159,16 @@ export default function Tabela() {
       });
   }
 
+  function arqui(){
+    setBtnArq(!btnArq);
+
+    let listAr = JSON.parse(localStorage.getItem('arquivado')|| '[]');
+    
+    setlistArq(listAr);
+
+
+
+  }
 
 
 
@@ -152,95 +185,208 @@ export default function Tabela() {
 
   return (
     <>
-      <Grid container>
-        <Grid item xs={12} >
-          <Container sx={{ mt: 7 }}>
-            <Button variant='contained' sx={{ mb: 2 }} onClick={() => setModalNovo(true)}> NOVO RECADO</Button>
-            <TableContainer component={Paper}>
-              <Table aria-label="customized table">
-                <TableHead>
-                  <TableRow>
-                    <StyledTableCell>#ID</StyledTableCell>
-                    <StyledTableCell align="center" >TÍTULO</StyledTableCell>
-                    <StyledTableCell align="center">DESCRIÇÃO</StyledTableCell>
-                    <StyledTableCell align="center">AÇÕES</StyledTableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
+    {!btnArq  ? (
+      <>
+            <Grid container>
+            <Grid item xs={12} >
+              <Container sx={{ mt: 7 }}>
+              <ToggleButton value="bold" aria-label="bold" onClick={()=> arqui()}>
+                   <DownloadForOfflineIcon />
+               </ToggleButton>
+             
+                <TableContainer component={Paper}>
+                  <Table aria-label="customized table">
+                    <TableHead>
+                      <TableRow>
+                        <StyledTableCell>#ID</StyledTableCell>
+                        <StyledTableCell align="center" >TÍTULO</StyledTableCell>
+                        <StyledTableCell align="center">DESCRIÇÃO</StyledTableCell>
+                        <StyledTableCell align="center">AÇÕES</StyledTableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+    
+                      {listArq.map((row: any) => (
+                        <StyledTableRow >
+                          <StyledTableCell >
+                            <Typography variant='h5'><b>{row.id}</b></Typography>
+                          </StyledTableCell>
+                          <StyledTableCell align="center">{row.titulo}</StyledTableCell>
+                          <StyledTableCell align="center">{row.descricao}
+                          </StyledTableCell>
+                          <StyledTableCell align="center" sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <Stack direction='row' spacing={2}>
+                              <IconButton onClick={() => deletarRecado(row.id)}>
+                                <DeleteForeverIcon sx={{ fontSize: 35 }} />
+                              </IconButton>
+                              <IconButton onClick={() => editarRecado(row.id, row.titulo, row.descricao)}>
+                                <EditIcon sx={{ fontSize: 35 }} />
+                              </IconButton>
+    
+                              <IconButton onClick={() => arquivado(row.id, row.titulo, row.descricao)}>
+                                <DownloadForOfflineIcon sx={{ fontSize: 35 }} />
+                              </IconButton>
+    
+                            </Stack>
+                          </StyledTableCell>
+                        </StyledTableRow>
+                      ))}
+    
+    
+    
+    
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Container>
+            </Grid>
+          </Grid>
+    
+    
+          {/* MODAL NOVO RECADO */}
+          <Dialog open={modalNovo}>
+            <DialogTitle>Novo recado</DialogTitle>
+            <Container>
+              <Typography variant='subtitle1'>Titulo</Typography>
+              <TextField onChange={(e) => setTitulo(e.target.value)} fullWidth />
+              <Typography variant='subtitle1'>Descricao</Typography>
+              <TextField onChange={(e) => setDescricao(e.target.value)} fullWidth />
+              <Container sx={{ mt: 2 }}>
+                <Stack direction="row" spacing={2} sx={{ mb: 3 }}>
+                  <Button variant="contained" onClick={() => salvarRecado()}>Salvar</Button>
+                  <Button variant="outlined" onClick={() => setModalNovo(false)}>Cancelar</Button>
+                </Stack>
+              </Container>
+            </Container>
+          </Dialog>
+          {/* Final modal nov recacado */}
+    
+    
+          {/* MODAL Editar */}
+          <Dialog open={modalEditar}>
+            <DialogTitle>Editar Recado</DialogTitle>
+            <Container>
+              <Typography variant='subtitle1'>Titulo</Typography>
+              <TextField value={titulo} onChange={(e) => setTitulo(e.target.value)} fullWidth />
+              <Typography variant='subtitle1'>Descricao</Typography>
+              <TextField value={descricao} onChange={(e) => setDescricao(e.target.value)} fullWidth />
+              <Container sx={{ mt: 2 }}>
+                <Stack direction="row" spacing={2} sx={{ mb: 3 }}>
+                  <Button variant="contained" onClick={() => salvarRecadoEditado()} > Salvar</Button>
+                  <Button variant="outlined" onClick={() => setModalEditar(false)}>Cancelar</Button>
+                </Stack>
+              </Container>
+            </Container>
+          </Dialog>
+          {/* Final ediatr */}
+    
+    
+    
+        </>
+    
+        ) : (
+          <>
 
-                  {lista.map((row: any) => (
-                    <StyledTableRow >
-                      <StyledTableCell >
-                        <Typography variant='h5'><b>{row.id}</b></Typography>
-                      </StyledTableCell>
-                      <StyledTableCell align="center">{row.titulo}</StyledTableCell>
-                      <StyledTableCell align="center">{row.descricao}
-                      </StyledTableCell>
-                      <StyledTableCell align="center" sx={{ display: 'flex', justifyContent: 'center' }}>
-                        <Stack direction='row' spacing={2}>
-                          <IconButton onClick={() => deletarRecado(row.id)}>
-                            <DeleteForeverIcon sx={{ fontSize: 35 }} />
-                          </IconButton>
-                          <IconButton onClick={() => editarRecado(row.id, row.titulo, row.descricao)}>
-                            <EditIcon sx={{ fontSize: 35 }} />
-                          </IconButton>
-                        </Stack>
-                      </StyledTableCell>
-                    </StyledTableRow>
-                  ))}
+
+<Grid container>
+            <Grid item xs={12} >
+              <Container sx={{ mt: 7 }}>
+              <ToggleButton value="bold" aria-label="bold" onClick={()=> arqui()}>
+                   <DownloadForOfflineIcon />
+               </ToggleButton>
+                <Button variant='contained' sx={{ mb: 2 }} onClick={() => setModalNovo(true)}> NOVO RECADO</Button>
+                <TableContainer component={Paper}>
+                  <Table aria-label="customized table">
+                    <TableHead>
+                      <TableRow>
+                        <StyledTableCell>#ID</StyledTableCell>
+                        <StyledTableCell align="center" >TÍTULO</StyledTableCell>
+                        <StyledTableCell align="center">DESCRIÇÃO</StyledTableCell>
+                        <StyledTableCell align="center">AÇÕES</StyledTableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+    
+                      {lista.map((row: any) => (
+                        <StyledTableRow >
+                          <StyledTableCell >
+                            <Typography variant='h5'><b>{row.id}</b></Typography>
+                          </StyledTableCell>
+                          <StyledTableCell align="center">{row.titulo}</StyledTableCell>
+                          <StyledTableCell align="center">{row.descricao}
+                          </StyledTableCell>
+                          <StyledTableCell align="center" sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <Stack direction='row' spacing={2}>
+                              <IconButton onClick={() => deletarRecado(row.id)}>
+                                <DeleteForeverIcon sx={{ fontSize: 35 }} />
+                              </IconButton>
+                              <IconButton onClick={() => editarRecado(row.id, row.titulo, row.descricao)}>
+                                <EditIcon sx={{ fontSize: 35 }} />
+                              </IconButton>
+    
+                              <IconButton onClick={() => arquivado(row.id, row.titulo, row.descricao)}>
+                                <DownloadForOfflineIcon sx={{ fontSize: 35 }} />
+                              </IconButton>
+    
+                            </Stack>
+                          </StyledTableCell>
+                        </StyledTableRow>
+                      ))}
+    
+    
+    
+    
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Container>
+            </Grid>
+          </Grid>
+    
+    
+          {/* MODAL NOVO RECADO */}
+          <Dialog open={modalNovo}>
+            <DialogTitle>Novo recado</DialogTitle>
+            <Container>
+              <Typography variant='subtitle1'>Titulo</Typography>
+              <TextField onChange={(e) => setTitulo(e.target.value)} fullWidth />
+              <Typography variant='subtitle1'>Descricao</Typography>
+              <TextField onChange={(e) => setDescricao(e.target.value)} fullWidth />
+              <Container sx={{ mt: 2 }}>
+                <Stack direction="row" spacing={2} sx={{ mb: 3 }}>
+                  <Button variant="contained" onClick={() => salvarRecado()}>Salvar</Button>
+                  <Button variant="outlined" onClick={() => setModalNovo(false)}>Cancelar</Button>
+                </Stack>
+              </Container>
+            </Container>
+          </Dialog>
+          {/* Final modal nov recacado */}
+    
+    
+          {/* MODAL Editar */}
+          <Dialog open={modalEditar}>
+            <DialogTitle>Editar Recado</DialogTitle>
+            <Container>
+              <Typography variant='subtitle1'>Titulo</Typography>
+              <TextField value={titulo} onChange={(e) => setTitulo(e.target.value)} fullWidth />
+              <Typography variant='subtitle1'>Descricao</Typography>
+              <TextField value={descricao} onChange={(e) => setDescricao(e.target.value)} fullWidth />
+              <Container sx={{ mt: 2 }}>
+                <Stack direction="row" spacing={2} sx={{ mb: 3 }}>
+                  <Button variant="contained" onClick={() => salvarRecadoEditado()} > Salvar</Button>
+                  <Button variant="outlined" onClick={() => setModalEditar(false)}>Cancelar</Button>
+                </Stack>
+              </Container>
+            </Container>
+          </Dialog>
+          {/* Final ediatr */}
+    
+
+          </>
 
 
-
-
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Container>
-        </Grid>
-      </Grid>
-
-
-      {/* MODAL NOVO RECADO */}
-      <Dialog open={modalNovo}>
-        <DialogTitle>Novo recado</DialogTitle>
-        <Container>
-          <Typography variant='subtitle1'>Titulo</Typography>
-          <TextField onChange={(e) => setTitulo(e.target.value)} fullWidth />
-          <Typography variant='subtitle1'>Descricao</Typography>
-          <TextField onChange={(e) => setDescricao(e.target.value)} fullWidth />
-          <Container sx={{ mt: 2 }}>
-            <Stack direction="row" spacing={2} sx={{ mb: 3 }}>
-              <Button variant="contained" onClick={() => salvarRecado()}>Salvar</Button>
-              <Button variant="outlined" onClick={() => setModalNovo(false)}>Cancelar</Button>
-            </Stack>
-          </Container>
-        </Container>
-      </Dialog>
-      {/* Final modal nov recacado */}
-
-
-      {/* MODAL Editar */}
-      <Dialog open={modalEditar}>
-        <DialogTitle>Editar Recado</DialogTitle>
-        <Container>
-          <Typography variant='subtitle1'>Titulo</Typography>
-          <TextField value={titulo} onChange={(e) => setTitulo(e.target.value)} fullWidth />
-          <Typography variant='subtitle1'>Descricao</Typography>
-          <TextField value={descricao} onChange={(e) => setDescricao(e.target.value)} fullWidth />
-          <Container sx={{ mt: 2 }}>
-            <Stack direction="row" spacing={2} sx={{ mb: 3 }}>
-              <Button variant="contained" onClick={() => salvarRecadoEditado()} > Salvar</Button>
-              <Button variant="outlined" onClick={() => setModalEditar(false)}>Cancelar</Button>
-            </Stack>
-          </Container>
-        </Container>
-      </Dialog>
-      {/* Final ediatr */}
-
-
-
-    </>
-
-
+        )}
+    
+</>
   );
 }
